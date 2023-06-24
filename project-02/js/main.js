b711488a4f99a7a9e62a92b28698873f9238a730
Rahul Saha <77447603+Rahul01866@users.noreply.github.com>
2023-06-24 05:59:11 -0700

    first commit

---
+++ project-02/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	
+	$(window).on("scroll", function(){
+		
+		if($(this).scrollTop() > 90){
+			$(".navbar").addClass("navbar-shrink");
+		}else{
+			$(".navbar").removeClass("navbar-shrink");
+		}
+		
+	});
+	
+	 AOS.init();
+	
+})
